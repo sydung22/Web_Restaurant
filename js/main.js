b8e5e6974c87de03c39b41ced8c004bc7e66c25f
sydung22 <72313449+sydung22@users.noreply.github.com>
2021-08-12 18:50:56 -0600

    add

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+
+$('.slider').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: false,
+   dots: true,
+   arrows: false,
+   autoplaySpeed: 3500,
+   speed: 1500,
+});
+  
+});
