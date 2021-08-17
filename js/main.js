7f7d1fc193283f17e5f1c580831184faa6d1a1b8
sydung22 <72313449+sydung22@users.noreply.github.com>
2021-08-17 16:39:50 -0600

    add

--- js/main.js
+++ js/main.js
@@ -6,9 +6,30 @@ $('.slider').slick({
   slidesToScroll: 1,
   autoplay: false,
    dots: true,
-   arrows: false,
+   arrows: true,
    autoplaySpeed: 3500,
    speed: 1500,
+   fade: true,
+   cssEase: 'linear'
+});
+$('.slide_aboutus').slick({
+  dots: true,
+  infinite: true,
+  speed: 1000,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  arrows: true,
+  prevArrow: '<i class="back text-dark ionicons ion-ios-arrow-left"></i>',
+  nextArrow: '',
+  // responsive: [{
+  //     breakpoint: 576,
+  //     settings: {
+  //         slidesToShow: 1,
+  //         slidesToScroll: 1,
+  //         infinite: true,
+  //         dots: true,
+  //     }
+  // }]
 });
   
 });
